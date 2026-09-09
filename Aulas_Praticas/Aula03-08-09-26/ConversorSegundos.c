cbedf8a79c35b0a60289c745f8e2288e289a6493
Rafael Andrade <rafacamara999@gmail.com>
2026-09-08 22:26:55 -0300

    ConversorSegundos

---
+++ Aulas_Praticas/Aula03-08-09-26/ConversorSegundos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+// lembrar do -lm no final por causa da biblioteca math.h
+
+int main () {
+    // declaracao de variaveis
+    int h, m, s, total = 0;
+
+    // entrada de dados
+    printf("Digite a hora, minutos e segundos:\n");
+    scanf("%d %d %d", &h, &m, &s);
+    
+    // processamento
+    total = (h * 3600) + (m * 60) + s;
+
+    // exibicao do resultado
+    printf("Total = %d\n", total);
+
+    return 0;
+}
